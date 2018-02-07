7396a8bedaabe77cff7db176f0e685d449cfe330
Administrator <Administrator@2013-20141206OQ>
2018-02-07 15:07:05 +0800

    ggfdgfd

--- src/com/lanou/redis/Demo1.java
+++ src/com/lanou/redis/Demo1.java
@@ -5,6 +5,7 @@ import redis.clients.jedis.Jedis;
 public class Demo1 {
 	public static void main(String[] args) {
 		//链接服务器
+		//gjgjhy
 		Jedis j = new Jedis("127.0.0.1",6379);
 		//权限认证
 		j.auth("123456");
